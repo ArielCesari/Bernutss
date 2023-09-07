3ba34ff999d8261f9c2200ce0e52e49e0f9f2fb5
ArielCesari <ariel.dcesari@gmail.com>
2023-09-07 19:04:11 -0300

    Comento lineas de código en archivo entry point

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
+/// ********* Require ´s  *********
 const express = require("express");
-const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+
+/// ********* Express  *********
+const app = express();
+
+/// ********* Middlewares  *********
+app.use(express.static("public"));// Necesario para los archivos estáticos en el folder /public
+app.use(methodOverride("_method"));  // Para poder usar los métodos PUT y DELETE
+app.use(express.urlencoded({ extended: false })); // Para poder recibir y manejar el body de los formularios como objetos literales
+app.use(express.json()); // Para poder recibir y manejar el body de los formularios como objetos literales
+
+// ************ Template Engine  ************
+app.set('views', path.resolve(__dirname, 'views')); // Define la ubicación de la carpeta de las Vistas
+app.set("view engine", "ejs")// Define que el motor que utilizamos es EJS
+
+// ************ Route System require and use()  ************
 const mainRouter = require("./routes/mainRouter.js");
 const productsRouter = require("./routes/productsRouter.js");
-
-
-
-app.use(express.static("public"));
-app.use(methodOverride("_method"));
-app.use(express.urlencoded({ extended: false }));
-
-
-app.set('views', path.resolve(__dirname, 'views'));
-app.set("view engine", "ejs")
 
 app.use("/", mainRouter);
 app.use('/products', productsRouter);
 
 
-
+// ************ Set the server to listen  ************
 app.listen(3001, (req, res) => {
 
     console.log("Servidor corriendo en http://localhost:3001")
